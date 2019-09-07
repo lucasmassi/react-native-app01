20f473fb94fe48779d935e7e73c85f848f9f99ce
Lucas Massi <lucasmassi18@gmail.com>
2019-09-07 00:55:54 -0300

    Add refreshing and loading status in listage

--- src/pages/User/styles.js
+++ src/pages/User/styles.js
@@ -75,3 +75,18 @@ export const Author = styled.Text`
   color: #666;
   margin-top: 2px;
 `;
+
+export const ActivityIndicator = styled.ActivityIndicator`
+  flex: 1;
+  margin-top: 30px;
+`;
+
+export const Loading = styled.ActivityIndicator.attrs({
+  color: '#7159c1',
+  size: 50,
+})`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`;
